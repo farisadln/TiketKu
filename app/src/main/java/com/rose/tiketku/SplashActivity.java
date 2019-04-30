2ce9805acae1969923a14d798df5e24187c1fa2a
farisdx66 <farisdx66@gmail.com>
2019-04-30 20:30:43 +0700

    Splash to GetStarted

---
+++ app/src/main/java/com/rose/tiketku/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.rose.tiketku;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent getstarted = new Intent(SplashActivity.this, GetStartedActivity.class);
+                startActivity(getstarted);
+                finish();
+            }
+        }, 1000);
+
+    }
+}
