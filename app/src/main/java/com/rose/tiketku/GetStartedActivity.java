3fd5b110de3ef06c5d77801b2bcffddb00f828ab
farisdx66 <farisdx66@gmail.com>
2019-05-04 15:52:27 +0700

    sign in layout

--- app/src/main/java/com/rose/tiketku/GetStartedActivity.java
+++ app/src/main/java/com/rose/tiketku/GetStartedActivity.java
@@ -1,13 +1,28 @@
 package com.rose.tiketku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GetStartedActivity extends AppCompatActivity {
+
+    Button btn_sign_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gosignin = new Intent(GetStartedActivity.this, SignInActivity.class);
+                startActivity(gosignin);
+            }
+        });
     }
 }
