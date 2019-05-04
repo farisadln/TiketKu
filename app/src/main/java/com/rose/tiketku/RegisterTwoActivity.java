4d2164d7a01ee17cbe71dd064b3d9e67a1f1b5b7
farisdx66 <farisdx66@gmail.com>
2019-05-04 20:13:59 +0700

    Layout register

--- app/src/main/java/com/rose/tiketku/RegisterTwoActivity.java
+++ app/src/main/java/com/rose/tiketku/RegisterTwoActivity.java
@@ -1,13 +1,16 @@
 package com.rose.tiketku;
 
 import android.content.Intent;
+import android.net.nsd.NsdManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class RegisterTwoActivity extends AppCompatActivity {
 
+    Button btn_continue;
     LinearLayout btn_back;
 
     @Override
@@ -16,12 +19,22 @@ public class RegisterTwoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register_two);
 
         btn_back = findViewById(R.id.btn_back);
+        btn_continue = findViewById(R.id.btn_continue);
+
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent goprev = new Intent(RegisterTwoActivity.this, RegisterOneActivity.class);
                 startActivity(goprev);
+            }
+        });
+
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gosucces = new Intent(RegisterTwoActivity.this, SuccessRegisterActivity.class);
+                startActivity(gosucces);
             }
         });
 
