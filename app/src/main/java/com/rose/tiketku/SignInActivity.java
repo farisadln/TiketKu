ee0dead3c2a3f2a4c4b774c4ef132c48c4576d66
farisdx66 <farisdx66@gmail.com>
2019-05-04 16:08:30 +0700

    Register one

--- app/src/main/java/com/rose/tiketku/SignInActivity.java
+++ app/src/main/java/com/rose/tiketku/SignInActivity.java
@@ -1,13 +1,28 @@
 package com.rose.tiketku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
+
+    TextView btn_new_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        btn_new_account = findViewById(R.id.btn_new_account);
+
+        btn_new_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goregist = new Intent(SignInActivity.this, RegisterOneActivity.class);
+                startActivity(goregist);
+            }
+        });
     }
 }
