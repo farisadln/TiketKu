b1e42f713d6bd01b786829aa5226377cc661ecf5
farisdx66 <farisdx66@gmail.com>
2019-05-05 18:39:31 +0700

    Layout register

---
+++ app/src/main/java/com/rose/tiketku/SuccessBuyTicketActivity.java
@@ -0,0 +1,13 @@
+package com.rose.tiketku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuccessBuyTicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_buy_ticket);
+    }
+}
